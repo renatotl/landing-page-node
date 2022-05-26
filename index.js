01a3d1d3ccd069ecbdfcab90496509f559f2ddbf
Renato Tenório Lino <103604513+renatotl@users.noreply.github.com>
2022-05-26 20:20:10 -0300

    up

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const express = require("express");// cria a variável express o require vai na pasta Node Modules procura a pasta express e traz tudo que ela tem
-const path = require("path")//importando uma lib do proprio express atraves do app
-const app = express();//cria a const app que é a aplicação e chama todaas as funções do express através deste app 
-app.set("view engine", "ejs");//motor engine da view é o ejs
-app.use(express.static(path.join(__dirname, "public")));//dizendo ao express  a pasta que ira guarda esses arquivos
+const express = require('express');//cria variável express, o require vai na pasta node modules procura a pasta express e traz tudo que ela tem ali dentro
+const path = require("path");//importando uma lib do proprio express chamada path
+const app = express();//cria a const app que é a aplicação e chama todas as funções do express atrasvés desse app
 
-app.get("/index", (req, res) => {//estamos chamando a rota get dentro domservidor que tem as rotas que o cliente está acessando e chamando os endpoints o cliente  as REQUISIÇÕES red que acessa o endpoint que é uma rota e dentro dessa rota é executaada uma função que RESPONDE res um cliente
-  res.render("index");
+app.set("view engine", "ejs"); //mortor engine da view é o ejs
+app.use(express.static(path.join(__dirname,"public")));//dizendo ao express a pasta que irá guardar esses arquivos
+
+app.get('/index', (req, res) => {//chamando a rota get, dentro do servidor que tem as rotas que o cliente está acessando o que chamamos de endpoints, o cliente faz a requisição e acessa o endpoint que é uma rota e dentro dessa rota é executada uma função e responde o meu cliente
+    const devList = ["BackEnd", "FrontEnd", "FullStack"];
+    const analyticsList = ["Engenharia de Dados", "Ciência de dados"];
+    res.render('index',{titulo: "Blue", devList: devList, analyticsList: analyticsList});
 });
 
-app.listen(3000, () => console.log("Servido está rodando em htpp:localhost:3000"));//ouvindo a porta 3000, rodando essa porta 
+app.post('/subscription', (req, res) => {
+    res.send("Formulário recebido!")
+});
+
+app.listen(3000, () => console.log("Servidor rodando em http://localhost:3000"));//ouvindo a porta 3000, rodando nessa porta
+
+
